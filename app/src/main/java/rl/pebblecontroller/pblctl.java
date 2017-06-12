e2a04090349e6c06d41c5cb3704dc84d6597b3b7
ricardo.larranaga@gmail.com <LGgthb123*>
2017-06-12 17:33:34 -0300

    This is the skeleton project. We will first get the service to start and stop from the main activity (Or to close from the Status bar). The we will start adding the proof of concept code.

---
+++ app/src/main/java/rl/pebblecontroller/pblctl.java
@@ -0,0 +1,16 @@
+package rl.pebblecontroller;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class pblctl extends Service {
+    public pblctl() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
